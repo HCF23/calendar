33e0a9b96c423401a7a94d2650331cdb96d9dfd0
Andrew Smith <46416133+HCF23@users.noreply.github.com>
2020-04-18 15:11:47 +0100

    init commit

---
+++ src/print.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define P(...) 		printf(__VA_ARGS__)
+#define DAY_MEM_SIZE 	(3) // char + char + \0
+#define PLACE 		(0)
+#define WIDTH		(20)
+#define MIN_WIDTH	(20)
+#define MAX_WIDTH	(132)
+
+char* day[] = {"Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"};
+
+struct months {
+	const char *name;
+	int days, start_wday, place;
+} month[12] = {
+		{ "January", 	31, 0, 0 },
+		{ "February", 	28, 0, 0 },
+		{ "March", 	31, 0, 0 },
+		{ "April", 	30, 0, 0 },
+		{ "May", 	31, 0, 0 },
+		{ "June", 	30, 0, 0 },
+		{ "July", 	31, 0, 0 },
+		{ "August", 	31, 0, 0 },
+		{ "September", 	30, 0, 0 },
+		{ "October", 	31, 0, 0 },
+		{ "November", 	30, 0, 0 },
+		{ "December", 	31, 0, 0 }
+};
+
+void space(int n) { while(n-- > 0) putchar(' '); }
+
+void print_month(int w, int i)
+{
+	space((w / 2));
+	P("%s\n", month[i].name);
+}
+
+void print_days(int start, int width)
+{
+	for (int i=start; i<=width; i+=DAY_MEM_SIZE)
+		P(" %s ", *(day)+i);
+	P("\n");		
+}
+
+void print_dates(void)
+{
+	while(1){
+		
+	}
+}
+
+int do_months_per_line(int width)
+{
+	if ((width < MIN_WIDTH) || (width > MAX_WIDTH))
+		return (-1);
+	return (width / 20);
+}
+
+void print_year(int )
+{
+	for (int m=0; m<12; m++){
+		print_month(print_width, m);
+		print_days(day_place, print_width);
+		print_dates();
+	}
+}
+
+int main(void)
+{
+	int day_place = PLACE;
+       	int print_width = WIDTH;
+	int months_per_line = do_months_per_line(print_width);
+	
+	if (months_per_line < 0) exit(1);
+
+	init_months();
+	print_year(months_per_line);
+	return 0;
+}
+
+
