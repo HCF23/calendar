4b2974bcc12af7e32d39bef899932343c981c5eb
A Smith <bitwiseoperator@protonmail.com>
2020-06-18 15:49:19 +0100

    add sakamoto alg for dotw

--- include/times.h
+++ include/times.h
@@ -1,11 +1,7 @@
 #include <stdbool.h>
 
-/*
- * init_months
- * 		gets current date
- */
+
 int get_year(void);
-
+int sakamoto_day_of_the_week(int year, int month);
 int gauss_weekday_jan1st(int year);
-
 bool leap_test(int year);
--- src/times.c
+++ src/times.c
@@ -24,6 +24,15 @@ int get_year(void){
 
 }
 
+int sakamoto_day_of_the_week(int year, int month) {
+	static int t[] = {0,3,2,5,0,3,5,1,4,6,2,4};
+	int y = year;
+	int m = month+1;
+	
+	y -= m < 3; //y-1 if jan or feb
+	return ((y + (y/4) - (y/100) + (y/400) + t[m-1] + 1)%7);
+}
+
 int gauss_weekday_jan1st(int year){
 /*
  * the weekday of jan1 in year Y
