f2df20989355a93b258b48653cf456bab9bb788a
A Smith <git_repo_slave@protonmail.com>
2020-06-19 18:05:50 +0100

    style

--- src/main.c
+++ src/main.c
@@ -11,33 +11,37 @@
 int term_width(void);
 int months_per_line(void);
 
-int main(int argc, char* argv[])
-{	
+int main(int argc, char *argv[])
+{
 	int year;
-	
-    if (!(argc))
-        exit(1);
-    
+
+    /* Rid output of compiler warns of unused argc */
+	if (!(argc))
+		exit(1);
+
 	if (argv[1] == NULL)
 		year = get_year();
 	else
 		year = atoi(argv[1]);
-	
+
 	print_year(months_per_line(), year);
 
 	return 0;
 }
 
-int months_per_line (void) {
+int months_per_line(void)
+{
 	return (do_months_per_line(term_width()) < 0)
-	? 1 : do_months_per_line(term_width());
-}	
+	    ? 1 : do_months_per_line(term_width());
+}
 
-int term_width(void) {
+int term_width(void)
+{
 	FILE *term;
 	int width;
-	
-	term = popen("tput cols","r");
+
+    /* Measure current terminal window width (cols) */
+	term = popen("tput cols", "r");
 	if (term != NULL) {
 		while (1) {
 			char *w;
@@ -50,10 +54,9 @@ int term_width(void) {
 		}
 		pclose(term);
 	} else {
+        /* Set width JIC */
 		width = 80;
 	}
-	
+
 	return width;
 }
-
-
